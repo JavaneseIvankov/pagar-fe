0e0266bea18a91a83facbb7ff2fa1bd1c9e4990f
arundaya <runsworkspace@gmail.com>
2026-03-13 23:17:29 +0700

    feat: added rpc and types folder

---
+++ rpc/index.ts
@@ -0,0 +1,4 @@
+"use server";
+
+// This file will encapsulate the RPC functions for the application
+// Mocking can be done here
---
+++ types/index.ts
@@ -0,0 +1,1 @@
+// NOTE: currently we'll stick with types/index.ts because i am not sure on how should we should structure types directory.

043004536f63418d6f164fe4575d3034d3fb735a
arundaya <runsworkspace@gmail.com>
2026-03-19 16:16:59 +0700

    feat(ui): use bucket images for auth thumbnails

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -11,13 +11,32 @@ const authContent: Record<
   }
 > = {
   "/auth/login": {
-    //  image: "/images/login.jpg",
-    image: "https://placehold.co/600x800",
+    image:
+      "https://bhnybebpsxnfyazsjxtk.supabase.co/storage/v1/object/public/pagar-assets/login-thumbnail.jpg.webp",
     title: "Welcome Back!",
     subtitle: "Log in to continue your journey.",
   },
   "/auth/register": {
-    image: "https://placehold.co/600x800",
+    image:
+      "https://bhnybebpsxnfyazsjxtk.supabase.co/storage/v1/object/public/pagar-assets/role-selection-thumbnail.jpg.webp",
+    title: "Join Us Today!",
+    subtitle: "Create an account to get started.",
+  },
+  "/auth/register/sppg": {
+    image:
+      "https://bhnybebpsxnfyazsjxtk.supabase.co/storage/v1/object/public/pagar-assets/register-sppg-thumbnail.jpg.webp",
+    title: "Join Us Today!",
+    subtitle: "Create an account to get started.",
+  },
+  "/auth/register/public": {
+    image:
+      "https://bhnybebpsxnfyazsjxtk.supabase.co/storage/v1/object/public/pagar-assets/register-public-thumbnail.jpg.webp",
+    title: "Join Us Today!",
+    subtitle: "Create an account to get started.",
+  },
+  "/auth/register/school": {
+    image:
+      "https://bhnybebpsxnfyazsjxtk.supabase.co/storage/v1/object/public/pagar-assets/register-school-thumbnail.jpg.webp",
     title: "Join Us Today!",
     subtitle: "Create an account to get started.",
   },
@@ -39,10 +58,11 @@ export default function AuthLayout({
           className="object-cover"
           priority
         />
-        <div className="absolute left-6 right-6 bottom-16 xl:bottom-24 xl:left-10 xl:right-10 text-white">
+        <div className="absolute left-6 right-6 bottom-16 xl:bottom-24 xl:left-10 xl:right-10 text-white z-10">
           <h1 className="text-h1">{content.title}</h1>
           <p className="text-body">{content.subtitle}</p>
         </div>
+        <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-black/70 to-transparent h-1/3" />
       </section>
       <section className="flex justify-center items-center mx-6">
         {children}

7c311301da6d1b06f62d431fcbe1b4871a554b16
arundaya <runsworkspace@gmail.com>
2026-03-20 15:42:02 +0700

    feat(report-tabs): linked report-tabs state to url path

--- src/components/report-tabs.tsx
+++ src/components/report-tabs.tsx
@@ -1,19 +1,32 @@
+"use client";
+
 import { FileVerifiedIcon, People } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+/**
+This component state is tied to pathanme
+*/
 export function ReportTabs() {
+  const pathname = usePathname();
+
   return (
-    <Tabs defaultValue="1">
+    <Tabs defaultValue="/public-report" value={pathname}>
       <TabsList>
-        <TabsTrigger value="1" className="flex gap-2">
-          <HugeiconsIcon icon={People} size={14} />
-          Laporan Masyarakat
+        <TabsTrigger value="/public-report" className="flex gap-2">
+          <Link href="/public-report">
+            <HugeiconsIcon icon={People} size={14} />
+            Laporan Masyarakat
+          </Link>
         </TabsTrigger>
 
-        <TabsTrigger value="2" className="flex gap-2">
-          <HugeiconsIcon icon={FileVerifiedIcon} size={14} />
-          Laporan SPPG
+        <TabsTrigger value="/sppg-report" className="flex gap-2">
+          <Link href="/sppg-report">
+            <HugeiconsIcon icon={FileVerifiedIcon} size={14} />
+            Laporan SPPG
+          </Link>
         </TabsTrigger>
       </TabsList>
     </Tabs>

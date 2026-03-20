78abe4f4a1686b5c1e752f1af3ea47da1c0e3763
arundaya <runsworkspace@gmail.com>
2026-03-20 17:43:34 +0700

    feat(nuqs): setup nuqs provider

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { debounce } from "nuqs";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { useState } from "react";
 import { Toaster } from "sonner";
 
@@ -15,8 +17,10 @@ export default function Providers({
   const [qc] = useState(() => createQueryClient());
   return (
     <QueryClientProvider client={qc}>
-      <Toaster />
-      {children}
+      <NuqsAdapter defaultOptions={{ limitUrlUpdates: debounce(500) }}>
+        <Toaster />
+        {children}
+      </NuqsAdapter>
     </QueryClientProvider>
   );
 }

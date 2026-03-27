4a8b9357294331fc911f7d8686a1b6c1c67bd64b
arundaya <runsworkspace@gmail.com>
2026-03-27 17:26:03 +0700

    feat: added utilities for delayed values

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,30 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function delayedValue<T>(value: T, delay: number = 1000): Promise<T> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value);
+    }, delay);
+  });
+}
+
+export function delayedValueCallback<T>(
+  value: T,
+  delay: number,
+): () => Promise<T> {
+  return () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(value);
+      }, delay);
+    });
+}
+
+export const truncateText = (text: string, maxLength: number) => {
+  if (text.length <= maxLength) {
+    return text;
+  }
+  return `${text.slice(0, maxLength)}...`;
+};

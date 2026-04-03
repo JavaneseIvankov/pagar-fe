8c8c16ddc409ccd1cad786845f9bc7ab7300c039
arundaya <runsworkspace@gmail.com>
2026-04-03 17:03:03 +0700

    fix: auth layout with sticky

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -64,7 +64,7 @@ export default function AuthLayout({
 
   return (
     <div className="grid min-h-dvh w-full grid-cols-1 bg-background md:grid-cols-2">
-      <section className="auth-thumbnail relative max-h-[10dvh] min-h-[240px] overflow-hidden bg-primary md:min-h-dvh">
+      <section className="auth-thumbnail relative relative top-0 left-0 max-h-[10dvh] min-h-[240px] overflow-hidden bg-primary md:sticky md:min-h-dvh">
         <motion.div
           key={content.image}
           className="absolute inset-0"
